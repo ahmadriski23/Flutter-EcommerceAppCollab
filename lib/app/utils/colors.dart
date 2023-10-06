725306ca3fd7127aaff6a9d92207a1790d9b40cc
Rizal Ihsan <rizal@rizalihsan.web.id>
2023-10-06 19:34:49 +0700

    utilities added

---
+++ lib/app/utils/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+abstract class AppColors {
+  static const purple = Color(0xff9775FA);
+  static const white = Color(0xffFEFEFE);
+  static const black = Color(0xff1D1E20);
+  static const gray = Color(0xff8F959E);
+  static const green = Color(0xff34C559);
+  static const orange = Color(0xffFF7043);
+  static const amber = Color(0xffFF981F);
+  static const red = Color(0xffFF5757);
+  static const facebook = Color(0xff4267B2);
+  static const twitter = Color(0xff1DA1F2);
+  static const google = Color(0xffEA4335);
+}
